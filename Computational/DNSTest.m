1fa12d56dede6b6dccdfd6981ec07056e610e9e4
David Simeroth <davidsimeroth@users.noreply.github.com>
2021-04-20 14:04:09 -0400

    Create DNSTest.m

---
+++ Computational/DNSTest.m
@@ -0,0 +1,28 @@
+%% DNSTest.m
+% arguments: N, L/H
+% Just a test script for analyzing performance of DNS for a hexagonal
+% enclosure. Will be updated to include a cylindrical occlusion.
+% N: number of subdivisions on each square division
+% L/H: edge to edge distance of hex divided by the height
+% Geometry is of form:
+%   |Y             |Z
+%  ___            ___
+% / O \  _X     || O || _X
+% \___/         ||___||
+%
+
+function [FWW,FWT,FWB,t]=DNSTest(N,L_H)
+tic
+H   = 1;         % Height of walls
+L   = H*L_H;     % Edge-to-edge distance of walls
+s   = L/sqrt(3); % Edge length of walls
+
+gsource = g_quad([-s/2,-L/2,-H/2],[0,0,H],[ s,0,0],N);
+% gtarget = g_quad([ s  ,   0,-H/2],[0,0,H],[-s/2,L/2,0],N);
+gtarget = g_hextest(N,L_H);
+
+FWW     = DNS(gsource,gtarget);
+FWT     = (1-FWW)/2;
+FWB     = FWT;
+t   = toc;
+end
